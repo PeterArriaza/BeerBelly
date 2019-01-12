05eb093024311e9a2df01c95d32ce52bc5d56dd1
PeterArriaza <peter.arriaza@gmail.com>
2019-01-12 16:52:11 -0500

    added footer with styling

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function watchSubmit() {
 }
 
 // make api call with user's values
-function getBeerData(city, state, index = 0, set = 0) {
+function getBeerData(city, state, index = 0) {
     var api_url = "http://beermapping.com/webservice/loccity/b79009d86aaf5ff13096058e40ac0780/" + city + "," + state + "&s=json";
     var settings = {
         url: api_url,
@@ -26,7 +26,7 @@ function getBeerData(city, state, index = 0, set = 0) {
         .done(function (result) {
             /* if the results are meeningful, we can just console.log them */
             var mapCenter = city + ',' + state;
-            positionMap(mapCenter, map, result, index, set);
+            positionMap(mapCenter, map, result, index);
         })
         /* if the call is NOT successful show errors */
         .fail(function (jqXHR, error, errorThrown) {
@@ -49,7 +49,8 @@ function initMap() {
     });
 }
 
-function positionMap(address, resultsMap, locations, index, set) {
+// center map on search city, state
+function positionMap(address, resultsMap, locations, index) {
     var geocoder = new google.maps.Geocoder();
     console.log(locations);
     geocoder.geocode({
@@ -57,7 +58,7 @@ function positionMap(address, resultsMap, locations, index, set) {
     }, function (results, status) {
         if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
-            sliceLocations(resultsMap, locations, index, set);
+            sliceLocations(resultsMap, locations, index);
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
@@ -65,7 +66,7 @@ function positionMap(address, resultsMap, locations, index, set) {
 }
 
 // split locations into arrays that do not cause query limit error in google geocoder
-function sliceLocations(map, locations, index, set) {
+function sliceLocations(map, locations, index) {
     // google maps geocoder api can only handle 10 calls in quick succession
     let size = 10;
     var points = [];
@@ -78,38 +79,32 @@ function sliceLocations(map, locations, index, set) {
     // remove beer graphic 
     $('#results').empty();
     console.log(points[0][0].city);
-    console.log(set);
+    // create values that can be passed to subsequent functions 
     $('#buttonDisplay').append(`
 <form>
 <input id="index" type="hidden" value="${(index+10)}"/>
-<input id="set" type="hidden" value="${(set+1)}"/>
 <input id="city" type="hidden" value="${points[0][0].city}"/>
 <input id="state" type="hidden" value="${points[0][0].state}"/>
 <button id="nextButton" class="hidden">Load Next 10 Results</button>
 </form>`);
-    displayMarkers(map, points, index, set);
+    displayMarkers(map, points, index);
 }
 
+// load next 10 search results on click 
 $(document).on('click', '#nextButton', function (event) {
     event.preventDefault();
     let indexValue = $(this).parent().find('#index').val();
     let cityVal = $(this).parent().find('#city').val();
     let stateVal = $(this).parent().find('#state').val();
-    let setVal = $(this).parent().find('#set').val();
 
-    getBeerData(cityVal, stateVal, indexValue, setVal);
+    getBeerData(cityVal, stateVal, indexValue);
 
 });
 
 // display makers on google map
-function displayMarkers(map, points, index, set) {
+function displayMarkers(map, points, index) {
     var geocoder = new google.maps.Geocoder();
     let i = 0;
-
-    //    if (set !== 0) {
-    //        i = set;
-    //    }
-    //    console.log(`lenghth is ${points[i].length}`);
 
     for (let j = 0; j < 10; j++) {
         console.log(points[i][j].street + ', ' + points[i][j].city + ', ' + points[i][j].state +
@@ -127,7 +122,7 @@ function displayMarkers(map, points, index, set) {
 
                 // show search results
                 $('#results').append(`<div class = "searchResult"><p class = "locationName">${points[i][j].name}</p>
-<p>Address: ${points[i][j].street + ', ' + points[i][j].city + ', ' + points[i][j].state +
+<p>${points[i][j].street + ', ' + points[i][j].city + ', ' + points[i][j].state +
             ', ' + points[i][j].zip}</p> 
 </div>`)
             } else {
@@ -137,6 +132,7 @@ function displayMarkers(map, points, index, set) {
 
         });
     }
+    // show button after results display to search for the next 10 locations
     $('#nextButton').removeClass('hidden');
 }
 
